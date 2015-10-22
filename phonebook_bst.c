39ed589dc07564617cd950eae716ae24082f433c
Gali Chen <w2812451@gmail.com>
2015-10-22 14:47:36 +0800

    phonebook binary searching tree version
    This version build a binary searching tree.Nonetheless, the input files
    is sorted, the tree will become very skew and takes more time to build
    the tree.

---
+++ phonebook_bst.c
@@ -0,0 +1,76 @@
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+#include IMPL
+
+#define max(a,b) ((a) > (b) ? (a) : (b))
+
+#define update(e) \
+    do { e->height = 1 + max(e->pLeft?e->pLeft->height:0, e->pRight?e->pRight->height:0); } while(0)
+
+entry *mallocEntry()
+{
+    entry *e = (entry *) malloc(sizeof(entry));
+    e->pLeft = e->pRight = NULL;
+    e->height = 0;
+    return e;
+}
+
+entry *_findName(char lastname[], entry *pHead)
+{
+    if (pHead->height == 0)
+        return NULL;
+
+    int cmp = strcmp(lastname, pHead->lastName);
+    if (cmp == 0)
+        return pHead;
+    else if (cmp < 0)
+        return _findName(lastname, pHead->pLeft);
+    else // cmp > 0
+        return _findName(lastname, pHead->pRight);
+}
+
+entry *_append(char lastName[], entry *e)
+{
+    if (e->height == 0) {
+        strcpy(e->lastName, lastName);
+        e->height = 1;
+        e->pLeft = mallocEntry();
+        e->pRight = mallocEntry();
+        return e;
+    }
+
+    int cmp = strcmp(lastName, e->lastName);
+    if (cmp < 0)
+        e->pLeft = _append(lastName, e->pLeft);
+    else if (cmp > 0)
+        e->pRight = _append(lastName, e->pRight);
+    else
+        return e;
+
+    update(e);
+    return e;
+}
+
+phonebook *create()
+{
+    phonebook *book = (phonebook *) malloc(sizeof(phonebook));
+    book->pHead = mallocEntry();
+    return book;
+}
+
+entry *findName(char lastname[], phonebook *book)
+{
+    return _findName(lastname, book->pHead);
+}
+
+entry *append(char lastName[], phonebook *book)
+{
+    entry *e = _append(lastName, book->pHead);
+    book->pHead = e;
+    return e;
+}
+
+#undef max
+#undef update
---
+++ phonebook_bst.h
@@ -0,0 +1,36 @@
+#ifndef _PHONEBOOK_H
+#define _PHONEBOOK_H
+
+#define MAX_LAST_NAME_SIZE 16
+
+/* original version */
+typedef struct __PHONE_BOOK_ENTRY {
+    char firstName[16];
+    char email[16];
+    char phone[10];
+    char cell[10];
+    char addr1[16];
+    char addr2[16];
+    char city[16];
+    char state[2];
+    char zip[5];
+} detail_entry;
+
+typedef struct __LAST_NAME_ENTRY {
+    char lastName[MAX_LAST_NAME_SIZE];
+    detail_entry *detail;
+
+    int height;
+    struct __LAST_NAME_ENTRY *pLeft;
+    struct __LAST_NAME_ENTRY *pRight;
+} entry;
+
+typedef struct __PHONE_BOOK {
+    entry *pHead; // root
+} phonebook;
+
+phonebook *create();
+entry *findName(char lastname[], phonebook *book);
+entry *append(char lastName[], phonebook *book);
+
+#endif
